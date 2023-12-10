57d06480a3047fe03ba0ebeb2d1a260345c3c248
César PM <cesar.pm.dev@gmail.com>
2023-12-10 01:18:45 -0500

    Agregando nestjs test

---
+++ src/tasks/dto/task.dto.ts
@@ -0,0 +1,28 @@
+
+import { Task, TaskStatus } from '../task.entity';
+import {IsNotEmpty, IsString, IsOptional, IsIn, MinLength } from 'class-validator'
+
+export class CreateTaskDto{
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    title: string;
+
+    @IsString()
+    description: string;
+}
+
+export class UpdateTaskDto{
+    @IsString()
+    @IsOptional()
+    title?: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+    status?: TaskStatus;
+}
